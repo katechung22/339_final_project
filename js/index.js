99e3819740e8a4a4675b9ce6a47d583fe0db70df
katechung22 <113209301+katechung22@users.noreply.github.com>
2024-04-11 15:22:26 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let images = ["../images/honkai_main_smaller.jpeg", "../images/league_main_smaller.jpeg", "../images/overwatch_main_smaller.jpeg", "../images/pokemon_main_smaller.jpeg", "../images/tft_main_smaller.jpeg", "../images/valorant_main_smaller.jpeg"]
+let alt = ["Honkai: Star Rail splash art", "League of Legends splash art", "Overwatch cover splash art", "Pokemon cover splash art", "TFT cover splash art", "Valorant cover splash art"]
+
+window.addEventListener("load", function() {
+	// let randomIndex = Math.floor(Math.random() * images.length);
+    // document.getElementById("randomImage").src = "../images/honkai_main_smaller.jpeg"
+    // document.getElementById("randomImage").alt = alt[randomIndex]
+    document.getElementById("randImage").style.backgroundImage = "url('../images/honkai_main_smaller.jpeg')"
+});
+
