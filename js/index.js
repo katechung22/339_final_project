47a7a1fe23d4380a29d118d508c41489ebf44b2f
katechung22 <113209301+katechung22@users.noreply.github.com>
2024-04-11 19:37:15 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let images = ["../images/honkai_main_smaller.jpeg", "../images/league_main_smaller.jpeg", "../images/overwatch_main_smaller.jpeg", "../images/pokemon_main_smaller.jpeg", "../images/tft_main_smaller.jpeg", "../images/valorant_main_smaller.jpeg"]
+let images = ["images/honkai_main.jpeg", "images/league_main.jpeg", "images/overwatch_main.jpeg", "images/pokemon_main.jpeg", "images/tft_main.jpeg", "images/valorant_main.jpeg"]
 let alt = ["Honkai: Star Rail splash art", "League of Legends splash art", "Overwatch cover splash art", "Pokemon cover splash art", "TFT cover splash art", "Valorant cover splash art"]
 
 window.addEventListener("load", function() {
